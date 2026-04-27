bc39717a0a3bed601a3a65ad1c14cdb867aadf52
timezonex <timezonex10@gmail.com>
2026-04-27 23:57:55 +1000

    implemented backed fetch of mc servers and services

--- backend/app.py
+++ backend/app.py
@@ -1,43 +1,124 @@
 import re
 import time
+from asyncio.windows_events import NULL
+
 from flask import Flask, render_template, redirect, jsonify
 from mcstatus import JavaServer
+from pathlib import Path
+import json
 
 app = Flask(__name__)
-server1_cache = {"data": None, "timestamp": 0}
+servers_cache = {"data": None, "timestamp": 0}
+services_cache = {"data": None, "timestamp": 0}
 CACHE_TTL = 30
 
-def get_server1_status():
-    now = time.time()
-    if now - server1_cache["timestamp"] < CACHE_TTL:
-        return server1_cache["data"]
 
-    try:
-        server = JavaServer.lookup("192.168.0.27")
-        status = server.status()
-        player_list = []
-        if status.players.sample:
-            for player in status.players.sample:
-                player_list.append({"name": player.name, "uuid": player.uuid})
-        data = {
-            "online": True,
-            "players_online": status.players.online,
-            "max_players": status.players.max,
-            "motd": status.description,
-            "player_list": player_list
-        }
-    except Exception as e:
-        data = {
-            "online": False,
-            "error": str(e)
-        }
-
-    server1_cache["data"] = data
-    server1_cache["timestamp"] = now
-    print(data)
-    return data
+class Configs:
+    def __init__(self, config_path):
+        self.config_path = Path(config_path)
+        self.configs = self.load_configs()
+        self.services_path = self.configs.get("services_path")
 
 
+
+    def load_configs(self):
+        with open(self.config_path, "r") as f:
+            config = json.load(f)
+            return config
+
+
+
+class Services:
+    def __init__(self):
+        pass
+
+    def load_services(self):
+        services_path = configs_handler.services_path
+        with open(services_path, "r") as f:
+            services = json.load(f)
+            self.services = services
+            self.minecraft_servers = [s for s in services if s['type'] == 'mc-server']
+            self.sites = [s for s in services if s['type'] == 'site']
+            print(f"services loaded: {services}")
+            print(f"minecraft servers loaded: {self.minecraft_servers}")
+            print(f"sites loaded: {self.sites}")
+
+
+    def get_minecraft_servers(self):
+        now = time.time()
+        if now - servers_cache["timestamp"] < CACHE_TTL:
+            return servers_cache["data"]
+
+        servers = self.minecraft_servers
+        servers_list = []
+        for server in servers:
+            data = {}
+            name = server['name']
+            edition = server['edition']
+            public_ip = server['public-ip']
+            public_port = server['public-port']
+            online = False
+
+            try:
+                server = JavaServer.lookup("192.168.0.27")
+                status = server.status()
+                player_list = []
+                if status.players.sample:
+                    for player in status.players.sample:
+                        player_list.append({"name": player.name, "uuid": player.uuid})
+
+                online = True
+                player_count = status.players.online
+                max_players = status.players.max
+                motd = status.description
+                data = {
+                    "name": name,
+                    "edition": edition,
+                    "public_ip": public_ip,
+                    "public_port": public_port,
+                    "online": online,
+                    "player_count": player_count,
+                    "max_players": max_players,
+                    "motd": motd
+                }
+
+
+            except Exception as e:
+                data = {
+                    "name": name,
+                    "edition": edition,
+                    "public_ip": public_ip,
+                    "public_port": public_port,
+                    "online": online,
+                    "error": str(e)
+                }
+
+            servers_list.append(data)
+
+        servers_cache["data"] = servers_list
+        servers_cache["timestamp"] = now
+        print(servers_cache)
+        return servers_cache
+
+
+    def get_services(self):
+        now = time.time()
+        if now - services_cache["timestamp"] < CACHE_TTL:
+            return services_cache["data"]
+
+        services_cache["data"] = self.services
+        servers_cache["timestamp"] = now
+        return services_cache
+
+
+
+    def get_sites(self):
+
+
+configs_handler = Configs("config.json")
+services_handler= Services()
+
+services_handler.load_services()
 @app.route('/')
 @app.route('/index')
 @app.route('/homepage')
@@ -45,15 +126,16 @@ def index():  # put application's code here
     return "hello world"
 
 
-@app.route('/api/server1_status')
-def server1_status():
-    try:
-        data = get_server1_status()
-        return jsonify(data)
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+@app.route('/api/servers')
+def get_servers():
+    servers = services_handler.get_minecraft_servers()
+    return jsonify(servers)
 
+
+@app.route('/api/services')
+def get_services():
+    services = services_handler.get_services()
+    return jsonify(services)
 
 if __name__ == '__main__':
     app.run(debug=True)
-    get_server1_status()
